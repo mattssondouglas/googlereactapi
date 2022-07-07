a10c179424a1f62e143d72ca044316180a0ee72a
Douglas <61967200+mattssondouglas@users.noreply.github.com>
2022-07-07 14:38:58 +0700

    modify last commit

--- models/results.js
+++ models/results.js
@@ -2,20 +2,20 @@ const mongoose = require('mongoose')
 
 // Create the results moodel
 mongoose.model('results', {
-	title: String,
-	required: true,
-	description: String,
-	required: true,
-	url: String,
-	required: true,
-	links: [
-		{
-			title: String,
-			url: String,
-		}
-		{
-			title: String,
-			url: String,
-		}
-	]
+  title: String,
+  required: true,
+  description: String,
+  required: true,
+  url: String,
+  required: true,
+  links: [
+    {
+      title: String,
+      url: String
+    },
+    {
+      title: String,
+      url: String
+    }
+  ]
 })
