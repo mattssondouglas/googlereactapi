4eeff82436176ed3434dc68d87c265e9a690e7dc
Douglas <61967200+mattssondouglas@users.noreply.github.com>
2022-07-07 14:38:31 +0700

    add schema to model/results.js

--- models/results.js
+++ models/results.js
@@ -1,4 +1,21 @@
 const mongoose = require('mongoose')
 
 // Create the results moodel
-mongoose.model('results', {})
+mongoose.model('results', {
+	title: String,
+	required: true,
+	description: String,
+	required: true,
+	url: String,
+	required: true,
+	links: [
+		{
+			title: String,
+			url: String,
+		}
+		{
+			title: String,
+			url: String,
+		}
+	]
+})
